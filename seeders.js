3f3315a2cef1cde1ca4f99637a0996d47a76755f
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-05-08 07:47:39 -0300

    Commented example code, moved files.js to lib, seeders.js private functions prefixed by underscore

--- seeders.js
+++ seeders.js
@@ -27,15 +27,15 @@ const Type = {
 const url = "magnet:?xt=urn:btih:568596006573d76622b99a16ee5fd2938a927a3a&dn=COMANDO.TO%20-%20Todas%20as%20Mulheres%20do%20Mundo%201%c2%aa%20Temporada%20Completa%202020%20%5b720p-FULL%5d&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=udp%3a%2f%2fglotorrents.pw%3a6969%2fannounce&tr=udp%3a%2f%2ftracker4.piratux.com%3a6969%2fannounce&tr=udp%3a%2f%2fcoppersurfer.tk%3a6969%2fannounce&tr=http%3a%2f%2ft2.pow7.com%2fannounce&tr=udp%3a%2f%2ftracker.yify-torrents.com%3a80%2fannounce&tr=http%3a%2f%2fwww.h33t.com%3a3310%2fannounce&tr=http%3a%2f%2fexodus.desync.com%2fannounce&tr=http%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=http%3a%2f%2fbt.careland.com.cn%3a6969%2fannounce&tr=http%3a%2f%2fexodus.desync.com%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.publicbt.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.istole.it%3a80%2fannounce&tr=http%3a%2f%2ftracker.blazing.de%2fannounce&tr=udp%3a%2f%2ftracker.openbittorrent.com%3a80%2fannounce&tr=http%3a%2f%2ftracker.yify-torrents.com%2fannounce&tr=udp%3a%2f%2ftracker.prq.to%2fannounce&tr=udp%3a%2f%2ftracker.1337x.org%3a80%2fannounce&tr=udp%3a%2f%2f9.rarbg.com%3a2740%2fannounce&tr=udp%3a%2f%2ftracker.ex.ua%3a80%2fannounce&tr=udp%3a%2f%2fipv4.tracker.harry.lu%3a80%2fannounce&tr=udp%3a%2f%2f12.rarbg.me%3a80%2fannounce&tr=udp%3a%2f%2ftracker.publicbt.com%3a80%2fannounce&tr=udp%3a%2f%2ftracker.opentrackr.org%3a1337%2fannounce&tr=udp%3a%2f%2f11.rarbg.com%2fannounce&tr=udp%3a%2f%2ftracker.ccc.de%3a80%2fannounce&tr=udp%3a%2f%2ffr33dom.h33t.com%3a3310%2fannounce&tr=udp%3a%2f%2fpublic.popcorn-tracker.org%3a6969%2fannounce";
 const torrentsInput = parseTorrent(url);
 
-async function getAllTorrentsTrackers(torrents) {
-  return Promise.all(torrents.map(torrent => getTorrentTrackers(torrent)
+async function _getAllTorrentsTrackers(torrents) {
+  return Promise.all(torrents.map(torrent => _getTorrentTrackers(torrent)
     .then(torrentTrackers => ({
       infoHash: torrent.infoHash,
       trackers: torrentTrackers
     }))))
 }
 
-function groupByTrackers(torrentsTrackers) {
+function _groupByTrackers(torrentsTrackers) {
   return torrentsTrackers.reduce((result, torrentTrackers) => {
       torrentTrackers.trackers.forEach(tracker =>
         result[tracker] = (result[tracker] || []).concat(torrentTrackers.infoHash));
@@ -44,7 +44,7 @@ function groupByTrackers(torrentsTrackers) {
 }
 
 
-function scrapeTrackersWithTimeout(mapTrackerInfoHash, callback) {
+function _scrapeTrackersWithTimeout(mapTrackerInfoHash, callback) {
   setTimeout(callback, SEEDS_CHECK_TIMEOUT);
 
   async.each(Object.keys(mapTrackerInfoHash), function (tracker, ready) {
@@ -72,12 +72,12 @@ function scrapeTrackersWithTimeout(mapTrackerInfoHash, callback) {
 }
 
 
-async function getSeedersPerTorrent(torrentsInput) {
+async function _getSeedersPerTorrent(torrentsInput) {
   return new Promise(async (resolve) => {
     const torrents = Array.isArray(torrentsInput) ? torrentsInput : [torrentsInput];
     const perTorrentResults = Object.fromEntries(new Map(torrents.map(torrent => [torrent.infoHash, {}])));
-    const torrentsTrackers = await getAllTorrentsTrackers(torrents);
-    const perTrackerInfoHashes = groupByTrackers(torrentsTrackers);
+    const torrentsTrackers = await _getAllTorrentsTrackers(torrents);
+    const perTrackerInfoHashes = _groupByTrackers(torrentsTrackers);
 
 
     let successfullTrackersCount = 0;
@@ -85,12 +85,12 @@ async function getSeedersPerTorrent(torrentsInput) {
       console.log(`[LOG] Total successful tracker responses: ${successfullTrackersCount}`)
       resolve(perTorrentResults);
     };
-    scrapeTrackersWithTimeout(perTrackerInfoHashes, callback);
+    _scrapeTrackersWithTimeout(perTrackerInfoHashes, callback);
   });
 }
 
 async function updateCurrentSeeders(torrents) {
-  let perTorrentResults = getSeedersPerTorrent(torrents);
+  let perTorrentResults = _getSeedersPerTorrent(torrents);
   const torrentsArray = Array.isArray(torrents) ? torrents : [torrents];
   torrentsArray.forEach(torrent => {
     const results = perTorrentResults[torrent.infoHash];
@@ -101,20 +101,20 @@ async function updateCurrentSeeders(torrents) {
   return torrents;
 }
 
-async function getTorrentTrackers(torrent) {
+async function _getTorrentTrackers(torrent) {
   const magnetTrackers = torrent.magnetLink && decode(torrent.magnetLink).tr || [];
   const torrentTrackers = torrent.trackers && torrent.trackers.split(',') || [];
-  const defaultTrackers = await getDefaultTrackers(torrent);
+  const defaultTrackers = await _getDefaultTrackers(torrent);
   return Array.from(new Set([].concat(magnetTrackers).concat(torrentTrackers).concat(defaultTrackers)));
 }
 
-async function getDefaultTrackers(torrent, retry = 3) {
+async function _getDefaultTrackers(torrent, retry = 3) {
   return needle('get', TRACKERS_URL, {
       open_timeout: SEEDS_CHECK_TIMEOUT
     })
     .then(response => response.body && response.body.trim())
     .then(body => body && body.split('\n\n') || [])
-    .catch(() => retry > 0 ? delay(5000).then(() => getDefaultTrackers(torrent, retry - 1)) : [])
+    .catch(() => retry > 0 ? delay(5000).then(() => _getDefaultTrackers(torrent, retry - 1)) : [])
     .then(trackers => trackers.concat(ADDITIONAL_TRACKERS))
     .then(trackers => torrent.type === Type.ANIME ? trackers.concat(ANIME_TRACKERS) : trackers);
 }
