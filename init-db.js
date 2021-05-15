14af55eeaf146825c446fc9717dc8c3190fb81d7
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-05-15 08:47:19 -0300

    Undo changes to init-db.js since commit 318212ff11f90c0624efba11ae415f7fd34587a8

--- init-db.js
+++ init-db.js
@@ -1,4 +1,20 @@
-const manifest =  require("./src/mico/persistence/models/stub/manifest.json");
+const manifest =  {
+	id: "brazilian-addon",
+	name: "Mico-Leão Dublado (BR addon)",
+	logo: "https://i.ibb.co/9tWdHsv/icon.jpg",
+	version: "0.2.0",
+	description: "Stremio addon for dubbed movies in portuguese (brazil).",
+	resources: ["catalog", "stream"],
+	types: ["movie", "series"],
+	catalogs: [{
+		type: "movie",
+		id: "BrazilianCatalog",
+		name: "Filmes Dublados (ptbr)",
+		genres: ["Ação", "Animação", "Aventura", "Clássico", "Comédia", "Documentário", "Drama", "Fantasia", "Ficção", "Faroeste", "Guerra", "Músicas", "Nacional", "Policial", "Romance", "Suspense", "Terror"],
+		extraSupported: ["search", "genre", "skip"]
+	}],
+	idPrefixes: ["tt"]
+}
 
 br = db.getSiblingDB('brazilian-stremio-addon');
 br.manifests.insert(manifest);
